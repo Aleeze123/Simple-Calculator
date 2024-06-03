696495a11a3411e159de738dc1d32510ebfd3ba7
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-06-04 04:20:42 +0500

    Add files via upload
    CLI based Simple Calculator

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.underline.yellow("\n\t\tWELCOME TO ALEEZE SIMPLE CALCULATOR...."));
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
     {
-        message: "Select one of the operators to perform operation",
+        message: chalk.underline.cyan("Enter first number"),
+        type: "number",
+        name: "firstNumber"
+    },
+    {
+        message: chalk.underline.cyan("Enter second number"),
+        type: "number",
+        name: "secondNumber"
+    },
+    {
+        message: chalk.underline.magenta("Select one of the operators to perform operation"),
         type: "list",
         name: "operator",
         choices: ["Addition", "Substraction", "Multiplication", "Division"],
@@ -24,5 +34,5 @@ else if (answer.operator === "Division") {
     console.log(answer.firstNumber / answer.secondNumber);
 }
 else {
-    console.log("Please select valid operator");
+    console.log(chalk.underline.red("Please select valid operator"));
 }
--- index.ts
+++ index.ts
@@ -1,11 +1,21 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
-
+import chalk from "chalk";
+console.log(chalk.underline.yellow("\n\t\tWELCOME TO ALEEZE SIMPLE CALCULATOR...."));
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
+   
+    { 
+        message: chalk.underline.cyan("Enter first number"),
+         type: "number", 
+         name: "firstNumber"
+         },
+    { 
+        message: chalk.underline.cyan("Enter second number"), 
+        type: "number",
+         name: "secondNumber"
+         },
     {
-      message: "Select one of the operators to perform operation",
+      message: chalk.underline.magenta("Select one of the operators to perform operation"),
       type: "list",
       name: "operator",
       choices: ["Addition", "Substraction", "Multiplication", "Division"],
@@ -15,13 +25,16 @@ const answer = await inquirer.prompt([
   //Conditional Test
   if (answer.operator === "Addition") {
     console.log(answer.firstNumber + answer.secondNumber);
-  } else if (answer.operator === "Substraction") {
-    console.log(answer.firstNumber - answer.secondNumber);
-  } else if (answer.operator === "Multiplication") {
-    console.log(answer.firstNumber * answer.secondNumber);
-  } else if (answer.operator === "Division") {
-    console.log(answer.firstNumber / answer.secondNumber);
-  } else {
-    console.log("Please select valid operator");
   } 
-
+  else if (answer.operator === "Substraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+  }
+   else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+  }
+   else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+  } 
+  else {
+    console.log(chalk.underline.red("Please select valid operator"));
+  } 
